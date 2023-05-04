47dce707f28e665943e8ee98c5deb0f58a44fc50
Lina Adamsson <studio@adamsson.com>
2023-05-04 23:08:06 +0200

    .

--- server.js
+++ server.js
@@ -20,9 +20,17 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Start defining your routes here. Adding path parameters.
 app.get("/books", (request, response) => {
-  const books = booksData;
+  const { title } = request.query;
+  let books = booksData;
+
+  if (title) {
+    books = booksData.filter((singleBook) => {
+      return singleBook.title.toLowerCase() === title.LowerCase();
+    });
+  }
+
   if (books) {
     response.status(200).json({
       success: true,
@@ -38,7 +46,34 @@ app.get("/books", (request, response) => {
       body: {}
     });
   }
-  // res.json(booksData);
+});
+
+// DRY 
+app.get("/books/:id", (request, response) => { // Always treated as a string.
+    // return book._title === request.params.id
+    const { id } = request.params;
+    console.log("id: ", title)
+    const singleBook = booksData.find((book) => {
+    // return book._title == id; // weak comp. not checking type.
+    return book._title === Number(id); // making id from parameter a number.
+    // return book._title.toString() === id;
+    // return book._id === +id;
+  });
+  if (singleBook) {
+    response.status(200).json({
+      success: true,
+      message: "OK",
+      body: {
+        booksData: singleBook
+      }
+    });
+  } else {
+    response.status(404).json({
+    success: false,
+      message: "Book not found",
+      body: {}
+    });
+  }
 });
 
 // get all technigo members
